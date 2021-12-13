a6b8ef6c24af179903314efcb86f756dfa3b2a51
Nuznhy <nuznhy@gmail.com>
2021-12-13 19:48:06 +0200

    multiplying

--- engine/app.cpp
+++ engine/app.cpp
@@ -1,10 +1,22 @@
 #include "app.hpp"
 
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/glm.hpp>
+
 #include <stdexcept>
 #include <array>
 #include <cstdlib>
 #include <ctime>
+
+
 namespace lvk {
+
+    struct SimplePushConstantData {
+        glm::vec2 offset;
+        alignas(16) glm::vec3 color;
+    };
+
     App::App(){
         loadModels();
         createPipelineLayout();
@@ -27,9 +39,9 @@ namespace lvk {
     void App::loadModels() {
         // srand (static_cast <unsigned> (time(0)));
         std::vector<LvkModel::Vertex> vertices {
-                {{1.0f,  -1.0f}},
-                {{-1.0f,  1.0f}},
-                {{-1.0f, -1.0f}},
+                {{0.0f,  -0.0f}},
+                {{0.5f,  0.5f}},
+                {{-0.5f, 0.5f}},
         };
 
         lvkModel = std::make_unique<LvkModel>(lvkDevice, vertices);
@@ -37,12 +49,18 @@ namespace lvk {
     }
 
     void App::createPipelineLayout() {
+
+        VkPushConstantRange pushConstantRange{};
+        pushConstantRange.stageFlags = VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT;
+        pushConstantRange.offset = 0;
+        pushConstantRange.size = sizeof(SimplePushConstantData);
+
         VkPipelineLayoutCreateInfo pipelineLayoutInfo{};
         pipelineLayoutInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
         pipelineLayoutInfo.setLayoutCount = 0;
         pipelineLayoutInfo.pSetLayouts = nullptr;
-        pipelineLayoutInfo.pushConstantRangeCount = 0;
-        pipelineLayoutInfo.pPushConstantRanges = nullptr;
+        pipelineLayoutInfo.pushConstantRangeCount = 1;
+        pipelineLayoutInfo.pPushConstantRanges = &pushConstantRange;
         if (vkCreatePipelineLayout(lvkDevice.device(), &pipelineLayoutInfo, nullptr, &pipelineLayout) != VK_SUCCESS){
             throw std::runtime_error("failed to create pipeline info");
         }
@@ -87,6 +105,9 @@ namespace lvk {
     }
 
     void App::recordCommandBuffer(int imageIndex) {
+        static  int frame = 0;
+        frame = (frame + 1) % 1000;
+
         VkCommandBufferBeginInfo beginInfo{};
         beginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
         if (vkBeginCommandBuffer(commandBuffers[imageIndex], &beginInfo) != VK_SUCCESS) {
@@ -101,7 +122,7 @@ namespace lvk {
         renderPathInfo.renderArea.extent = lvkSwapChain->getSwapChainExtent();
 
         std::array<VkClearValue, 2> clearValues{};
-        clearValues[0].color = {0.1f, 0.1f, 0.1f, 1.0f};
+        clearValues[0].color = {0.01f, 0.01f, 0.01f, 1.0f};
         clearValues[1].depthStencil = {1.0f, 0};
 
         renderPathInfo.clearValueCount = static_cast<uint32_t>(clearValues.size());
@@ -122,7 +143,17 @@ namespace lvk {
 
         lvkPipeline->bind(commandBuffers[imageIndex]);
         lvkModel->bind(commandBuffers[imageIndex]);
-        lvkModel->draw(commandBuffers[imageIndex]);
+
+        for (int j = 0; j < 4; j++) {
+            SimplePushConstantData push{};
+            push.offset = {-0.5f + frame * 0.00002f, -0.5f + j * 0.25};
+            push.color = {0.0f, 0.0f, 0.2f * j + 0.2f};
+            vkCmdPushConstants(commandBuffers[imageIndex], pipelineLayout,
+                               VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT, 0,
+                               sizeof(SimplePushConstantData), &push);
+            lvkModel->draw(commandBuffers[imageIndex]);
+        }
+
 
         vkCmdEndRenderPass(commandBuffers[imageIndex]);
         if (vkEndCommandBuffer(commandBuffers[imageIndex]) != VK_SUCCESS) {
--- src/shaders/shader.frag
+++ src/shaders/shader.frag
@@ -1,8 +1,12 @@
 #version 450
-#extension GL_ARB_separate_shader_objects : enable
+
 
 layout (location = 0) out vec4 outColor;
+layout(push_constant) uniform Push {
+        vec2 offset;
+        vec3 color;
+} push;
 
 void main(){
-    outColor = vec4(1.0, 1.0, 0, 1.0);
+    outColor = vec4(push.color, 1.0);
 }
