0474962c0c835e833ea72d6755b80fd403bb36f9
nuznhy <nuznhy@gmail.com>
2021-12-16 04:18:25 +0200

    uptimized models loading

--- engine/app.cpp
+++ engine/app.cpp
@@ -56,60 +56,13 @@ namespace lvk {
         vkDeviceWaitIdle(lvkDevice.device());
     }
 
-    std::unique_ptr<LvkModel> createCubeModel(LvkDevice& device, glm::vec3 offset) {
-        LvkModel::Builder modelBuilder{};
-        modelBuilder.vertices =
-            {
-                // left face (white)
-                {{-.5f, -.5f, -.5f}, {.9f, .9f, .9f}},
-                {{-.5f, .5f, .5f}, {.9f, .9f, .9f}},
-                {{-.5f, -.5f, .5f}, {.9f, .9f, .9f}},
-                {{-.5f, .5f, -.5f}, {.9f, .9f, .9f}},
-
-                // right face (yellow)
-                {{.5f, -.5f, -.5f}, {.8f, .8f, .1f}},
-                {{.5f, .5f, .5f}, {.8f, .8f, .1f}},
-                {{.5f, -.5f, .5f}, {.8f, .8f, .1f}},
-                {{.5f, .5f, -.5f}, {.8f, .8f, .1f}},
-
-                // top face (orange, remember y axis points down)
-                {{-.5f, -.5f, -.5f}, {.9f, .6f, .1f}},
-                {{.5f, -.5f, .5f}, {.9f, .6f, .1f}},
-                {{-.5f, -.5f, .5f}, {.9f, .6f, .1f}},
-                {{.5f, -.5f, -.5f}, {.9f, .6f, .1f}},
-
-                // bottom face (red)
-                {{-.5f, .5f, -.5f}, {.8f, .1f, .1f}},
-                {{.5f, .5f, .5f}, {.8f, .1f, .1f}},
-                {{-.5f, .5f, .5f}, {.8f, .1f, .1f}},
-                {{.5f, .5f, -.5f}, {.8f, .1f, .1f}},
-
-                // nose face (blue)
-                {{-.5f, -.5f, 0.5f}, {.1f, .1f, .8f}},
-                {{.5f, .5f, 0.5f}, {.1f, .1f, .8f}},
-                {{-.5f, .5f, 0.5f}, {.1f, .1f, .8f}},
-                {{.5f, -.5f, 0.5f}, {.1f, .1f, .8f}},
-
-                // tail face (green)
-                {{-.5f, -.5f, -0.5f}, {.1f, .8f, .1f}},
-                {{.5f, .5f, -0.5f}, {.1f, .8f, .1f}},
-                {{-.5f, .5f, -0.5f}, {.1f, .8f, .1f}},
-                {{.5f, -.5f, -0.5f}, {.1f, .8f, .1f}},
-            };
-        for (auto& v : modelBuilder.vertices) {
-            v.position += offset;
-        }
-        modelBuilder.indices = {0,  1,  2,  0,  3,  1,  4,  5,  6,  4,  7,  5,  8,  9,  10, 8,  11, 9,
-                                12, 13, 14, 12, 15, 13, 16, 17, 18, 16, 19, 17, 20, 21, 22, 20, 23, 21};
-        return std::make_unique<LvkModel>(device, modelBuilder);
-    }
 
     void App::loadGameObjects() {
-        std::shared_ptr<LvkModel> lvkModel = createCubeModel(lvkDevice, {.0f, .0f, .0f});
-        auto cube = LvkGameObject::createGameObject();
-        cube.model = lvkModel;
-        cube.transform.translation = {.0f, .0f, 2.5f};
-        cube.transform.scale = {.5f, .5f, .5f};
-        gameObjects.push_back(std::move(cube));
+        std::shared_ptr<LvkModel> lvkModel = LvkModel::createModelFromFile(lvkDevice, "/home/nuznhy/CLionProjects/VulkanProject/src/models/smooth_vase.obj");
+        auto gameObj = LvkGameObject::createGameObject();
+        gameObj.model = lvkModel;
+        gameObj.transform.translation = {.0f, .0f, 2.5f};
+        gameObj.transform.scale = glm::vec3(3.f);
+        gameObjects.push_back(std::move(gameObj));
     }
 }
--- engine/lvk_model.cpp
+++ engine/lvk_model.cpp
@@ -1,7 +1,27 @@
 #include "lvk_model.hpp"
+#include "lvk_utils.hpp"
+
+#define TINYOBJLOADER_IMPLEMENTATION
+#include <tiny_obj_loader.h>
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/gtx/hash.hpp>
 
 #include <cstring>
 #include <cassert>
+#include <iostream>
+#include <unordered_map>
+
+namespace std{
+    template<>
+    struct hash<lvk::LvkModel::Vertex> {
+        size_t operator()(lvk::LvkModel::Vertex const &vertex) const {
+            size_t seed = 0;
+            lvk::hashCombine(seed, vertex.position, vertex.color, vertex.normal, vertex.uv);
+            return seed;
+        }
+    };
+}
+
 namespace lvk {
 
     LvkModel::LvkModel(LvkDevice &device, const LvkModel::Builder &builder) : lvkDevice{device} {
@@ -16,6 +36,13 @@ namespace lvk {
             vkDestroyBuffer(lvkDevice.device(), indexBuffer, nullptr);
             vkFreeMemory(lvkDevice.device(), indexBufferMemory, nullptr);
         }
+    }
+
+    std::unique_ptr<LvkModel> LvkModel::createModelFromFile(LvkDevice &device, const std::string &filepath){
+        Builder builder{};
+        builder.loadModel(filepath);
+        std::cout << "Vertex count: " << builder.vertices.size() << std::endl;
+        return std::make_unique<LvkModel>(device, builder);
     }
 
     void LvkModel::createVertexBuffers(const std::vector<Vertex> &vertices) {
@@ -119,5 +146,64 @@ namespace lvk {
         attributeDescriptions[1].offset = offsetof(Vertex, color);
 
         return attributeDescriptions;
+    }
+
+    void LvkModel::Builder::loadModel(const std::string &filepath) {
+        tinyobj::attrib_t attrib;
+        std::vector<tinyobj::shape_t> shapes;
+        std::vector<tinyobj::material_t> materials;
+        std::string warn, err;
+
+        if (!tinyobj::LoadObj(&attrib, &shapes, &materials, &warn, &err, filepath.c_str())) {
+            throw std::runtime_error(warn + err);
+        }
+
+        vertices.clear();
+        indices.clear();
+
+        std::unordered_map<Vertex, uint32_t> uniqueVertices{};
+        for(const auto &shape : shapes) {
+            for (const auto &index : shape.mesh.indices) {
+                Vertex vertex{};
+                if (index.vertex_index >= 0 ) {
+                    vertex.position = {
+                            attrib.vertices[3 * index.vertex_index + 0],
+                            attrib.vertices[3 * index.vertex_index + 1],
+                            attrib.vertices[3 * index.vertex_index + 2]
+                    };
+
+                    auto colorIndex = 3 * index.vertex_index + 2;
+                    if (colorIndex < attrib.colors.size()) {
+                        vertex.color = {
+                                attrib.colors[colorIndex - 2],
+                                attrib.colors[colorIndex - 1],
+                                attrib.colors[colorIndex - 0]
+                        };
+                    } else {
+                        vertex.color = {1.f, 1.f, 1.f};
+                    }
+                }
+
+                if (index.normal_index >= 0 ) {
+                    vertex.normal = {
+                            attrib.normals[3 * index.normal_index + 0],
+                            attrib.normals[3 * index.normal_index + 1],
+                            attrib.normals[3 * index.normal_index + 2]
+                    };
+                }
+
+                if (index.texcoord_index >= 0 ) {
+                    vertex.uv = {
+                            attrib.texcoords[2 * index.texcoord_index + 0],
+                            attrib.texcoords[2 * index.texcoord_index + 1]
+                    };
+                }
+                if (uniqueVertices.count(vertex) == 0) {
+                    uniqueVertices[vertex] = static_cast<uint32_t>(vertices.size());
+                    vertices.push_back(vertex);
+                }
+                indices.push_back(uniqueVertices[vertex]);
+            }
+        }
     }
 }
